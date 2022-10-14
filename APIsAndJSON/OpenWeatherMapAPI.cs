10363ded9fe4f800eddee1c2b07d1eb1ff0074e6
ChrisJonesTC <cfjones91@gmail.com>
2022-10-14 09:17:03 -0400

    API JSON

--- APIsAndJSON/OpenWeatherMapAPI.cs
+++ APIsAndJSON/OpenWeatherMapAPI.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,17 @@ namespace APIsAndJSON
 {
     internal class OpenWeatherMapAPI
     {
+        public static void PhillyTemp()
+        {
+            //var key = "bcbcabeea971ba9430a7d0c2ec63cd36";
+            //var city = "Philadelphia";
+            var client = new HttpClient();
+            var weatherURL = "https://api.openweathermap.org/data/2.5/forecast?q=Philadelphia&units=imperial&appid=bcbcabeea971ba9430a7d0c2ec63cd36";
+            var jsonResponse = client.GetStringAsync(weatherURL).Result;
+            JObject formattedResponse = JObject.Parse(jsonResponse);
+            var temp = formattedResponse["list"][0]["main"]["temp"];
+            Console.WriteLine(temp);
+        }
+       
     }
 }
--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+          
+            RonVSKanyeAPI.KanyeQuote();
+            Console.WriteLine();
+            RonVSKanyeAPI.RonQuote();
+            Console.WriteLine("---------------");
+            RonVSKanyeAPI.KanyeQuote();
+            Console.WriteLine();
+            RonVSKanyeAPI.RonQuote();
+            Console.WriteLine("---------------");
+            RonVSKanyeAPI.KanyeQuote();
+            Console.WriteLine();
+            RonVSKanyeAPI.RonQuote();
+            Console.WriteLine("---------------");
+            RonVSKanyeAPI.KanyeQuote();
+            Console.WriteLine();
+            RonVSKanyeAPI.RonQuote();
+            Console.WriteLine("---------------");
+            RonVSKanyeAPI.KanyeQuote();
+            Console.WriteLine();
+            RonVSKanyeAPI.RonQuote();
+            Console.WriteLine("---------------");
+ 
+
+            OpenWeatherMapAPI.PhillyTemp();
         }
     }
 }
